c49b80e8ee674e13bfff590b4e2364bcfa62d81b
Artur Smirnov <typeks@gmail.com>
2022-08-28 20:11:47 +0300

    Исправлена матрица с фиксированного на произвольный размер.

--- Homework_03/main.cpp
+++ Homework_03/main.cpp
@@ -8,6 +8,7 @@
 #include <tuple>
 #include <algorithm>
 #include <windows.h>
+// #include "Timer.h"
 
 void Middle(std::list<double> &list)
 {
@@ -25,26 +26,44 @@ template <class T>
 class Matrix
 {
 private:
-	std::vector<std::vector<T>> mat;
+	size_t matrixSize = 0;
+	std::vector<std::vector<double>> mat;
 public:
-	Matrix(T a1, T a2, T a3, T b1, T b2, T b3, T c1, T c2, T c3)
+	Matrix(){}
+	Matrix(size_t matrixSize, std::vector<T>& vec) : matrixSize(matrixSize)
 	{
-			this->mat.push_back(std::vector<T>{a1,a2,a3});
-			this->mat.push_back(std::vector<T>{b1,b2,b3});
-			this->mat.push_back(std::vector<T>{c1,c2,c3});
+		for (size_t iter1 = 0; iter1 < matrixSize; iter1++)
+		{
+			std::vector<double> temp;
+			for(size_t iter2 = 0; iter2 < matrixSize; iter2++)
+			{
+				temp.push_back((double)vec[iter1*matrixSize+iter2]);
+			}
+			this->mat.push_back(temp);
+			temp.clear();
+		}
 	}
 	T GetDeterminant()
 	{
-		T determinant = 0;
-		for (size_t iter = 0; iter < 3; iter++)
+		double multiplier = .0;
+		std::vector<std::vector<double>> temp(mat);
+		for (size_t iter1 = 1,iter2 = 0; iter1 < matrixSize; iter1++,iter2++)
 		{
-			determinant += mat[0][iter] * mat[1][(iter + 1) % 3] * mat[2][(iter + 2) % 3];
+			for (size_t iter4 = iter1; iter4 < matrixSize; iter4++)
+			{
+				multiplier = temp[iter4][iter2] / temp[iter1 - 1][iter2];
+				for (size_t iter3 = 0; iter3 < matrixSize; iter3++)
+				{
+					temp[iter4][iter3] = temp[iter4][iter3] - temp[iter1 - 1][iter3] * multiplier;
+				}
+			}
 		}
-		for (size_t iter = 0; iter < 3; iter++)
+		double result = temp[0][0];
+		for (size_t i = 1; i < matrixSize; i++)
 		{
-			determinant -= mat[0][(iter + 2) % 3] * mat[1][(iter + 1) % 3] * mat[2][iter];
+			result *= temp[i][i];
 		}
-		return determinant;
+		return (T)result;
 	}
 };
 
@@ -83,9 +102,17 @@ int main(int argc, char** argv)
 
 	////////////////////////////////
 
-	Matrix m(1, 2, 2, 4, 5, 4, 7, 8, 9);
+	std::vector vec1 = {2.,2.,2.,0.,3.,5.,5.,3.,1.,3.,2.,0.,1.,0.,3.,2.};
+	Matrix m1(4, vec1);
 
-	std::cout << m.GetDeterminant() << std::endl;
+	std::cout.precision(2);
+	std::cout << m1.GetDeterminant() << std::endl;
+	std::cout << std::endl;
+
+	std::vector vec2 = {2.,2.,2.,0.};
+	Matrix m2(2, vec2);
+
+	std::cout << m2.GetDeterminant() << std::endl;
 	std::cout << std::endl;
 
 	///////////////////////////////
